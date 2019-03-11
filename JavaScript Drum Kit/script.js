f2b583f0308b5e8265c9c66ea1ba17095ee268c0
Liam Dawson <liamrdawson@gmail.com>
2019-03-11 21:35:06 +0000

    add and remove animation to match sound on keydown

--- JavaScript Drum Kit/script.js
+++ JavaScript Drum Kit/script.js
@@ -1,6 +1,20 @@
-window.addEventListener('keydown', function(e){
+
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(audio);
     if (!audio) return; // Stop function from running if no audio available
+    audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('playing');
+}
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') { return; } 
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
