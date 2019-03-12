e15a07894025d9a7a95c14bd177c12c107811612
Liam Dawson <liamrdawson@gmail.com>
2019-03-12 20:44:07 +0000

    set hand rotation to setInterval for every second, min, hour

--- JS and CSS Clock/script.js
+++ JS and CSS Clock/script.js
@@ -1,1 +1,20 @@
-console.log("Hi from script.js :D");
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+// E.G. 60 / 60 = 1, then multiplied by 360 = 360, will place the degree of rotation correctly.
+// '+ 90' to offset default transform: rotate value.
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minDegrees = ((minutes / 60) * 360) + 90;
+    const hourDegrees = ((minutes / 12) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
