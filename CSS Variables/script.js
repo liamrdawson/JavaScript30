74805bfc68e9f8b16a388f885d7cd6df585420dd
Liam Dawson <liamrdawson@gmail.com>
2019-03-12 22:20:01 +0000

    add JS so that this.input will change CSS variables

--- CSS Variables/script.js
+++ CSS Variables/script.js
@@ -1,1 +1,10 @@
-console.log("hi from script.js!")
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+//  this.dataset refers to the 'data-' value stored in the element, in this case; px as we're using dataSet.sizing or 'data-sizing="px"'. Add 'or nothing ||' to avoid an undefined value.
+    const suffix = this.dataset.sizing || ''; 
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
