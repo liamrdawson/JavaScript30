de723cffa7b2e767eca7fe19dd29d8e2a60b3574
Liam Dawson <liamrdawson@gmail.com>
2019-03-13 21:21:21 +0000

    refactor Array.prototype.sort() using arrow syntax and ternererererary operators

--- Array Cardio Day 1/script.js
+++ Array Cardio Day 1/script.js
@@ -33,14 +33,7 @@ console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-const ordered = inventors.sort(function(a, b){
-    if (a.year > b.year) {
-        return 1;
-    } else {
-        return -1;
-    } 
-});
-
+const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
 console.log(ordered);
 
 // Array.prototype.reduce()
