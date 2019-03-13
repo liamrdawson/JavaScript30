843bf2e5d9d3267826faa54ebf3b8f662bafa4a9
Liam Dawson <liamrdawson@gmail.com>
2019-03-13 22:41:17 +0000

    sum up the total instances of each object property using .reduce()

--- Array Cardio Day 1/script.js
+++ Array Cardio Day 1/script.js
@@ -74,3 +74,12 @@ console.log(alpha);
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+const transport = data.reduce(function(obj, item){
+    if (!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+}, {});
+console.log(transport);
